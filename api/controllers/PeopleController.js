73e8535ec500c5b2d13e0267359ce3272b47295f
Davi Ramon <davi.goncalves@solarview.com.br>
2022-05-28 22:54:15 -0300

    adicionando método para criar  novo aluno ou professor

--- api/controllers/PeopleController.js
+++ api/controllers/PeopleController.js
@@ -2,17 +2,82 @@ const database = require('../models'); // vai direto ao arquivo index.js dentro 
 
 
 class PeopleController {
-     static async getAllPeople(req, res) {
-          try {
 
+
+     static async getAllPeople(req, res) {
+
+          try {
                const allPeople = await database.People.findAll(); // findall é um método do sequelize que faz igual um select * from.. traz tudo.;
                return res.status(200).json(allPeople);
 
           } catch (error) {
-               console.error(error);
                return res.status(500).json(error.message);
           }
      }
+
+
+     static async getOnePerson(req, res) {
+
+          const { id } = req.params;
+
+          try {                    // primeiro ID é a coluna com o nome ID
+               const onePerson = await database.People.findOne({ where: { id: Number(id) } }) // findone é um método do sequelize que fbusca somente 1 registro. passado pelo ID. 
+               return res.status(200).json(onePerson);
+
+          } catch (error) {
+               return res.status(500).json(error.message);
+          }
+     }
+
+
+     static async createPerson(req, res) {
+
+          const newPerson = req.body // armazena os dadoso do corpo da requisição
+
+          try {
+               const createdNewPerson = await database.People.create(newPerson);
+               return res.status(200).json(createdNewPerson);
+
+          } catch (error) {
+               
+               return res.status(500).json(error.message);
+          }
+
+
+     }
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = PeopleController;
--- api/routes/peopleRoute.js
+++ api/routes/peopleRoute.js
@@ -4,5 +4,7 @@ const PeopleController = require('../controllers/PeopleController');
 const router = Router();
 
 router.get('/people', PeopleController.getAllPeople);
+router.get('/people/:id', PeopleController.getOnePerson);
+router.post('/people', PeopleController.createPerson);
 
 module.exports = router;
